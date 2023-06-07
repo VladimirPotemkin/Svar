0fcc74e68b83b60b5b2450b5fbd09f74ea12d684
Vladimir Potemkin <68517705+VladimirPotemkin@users.noreply.github.com>
2023-06-07 08:31:34 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,641 @@
+<template>
+
+<div class="page">
+    <header class="header">
+        <div class="header--title">
+        <button class="header--btn">
+            <img src="./img/noun-phone-52971.png" alt="" class="phone--img">
+            <p>Показать Телефон</p>
+        </button>
+        <div class="header--text">
+            <img src="./img/noun-clock-5759930.png" alt="" class="clock--img">
+            <p>Пн - Пт с 9:00 до 18:00</p>
+            </div>
+        </div>
+        <div class="header--menu">
+        <img src="./img/logo.png" class="menu--logo" alt="">
+        <h3>СВАРМЕТАЛЛ</h3>
+        <div class="menu--list">
+            <div class="menu--item">Производство</div>
+            <div class="menu--item">Контакты</div>
+            <div class="menu--item">Услуги</div>
+            <div class="menu--item">Скидки</div>
+        </div>
+        <button class="menu--btn">Заказать звонок</button>
+    </div>
+    </header>
+
+    
+
+    <div class="store">
+        <h1 class="store--title">Изготовление пищевых ёмкостей на заказ</h1>
+        <h4 class="store--text">Серийные и штучные заказы</h4>
+        <h4 class="store--text">По вашим чертежам и эскизам</h4>
+        <h4 class="store--text">Гарантия 1 год</h4>
+        <h4 class="store--text"><span class="text--line"></span>Гибкие цены</h4>
+        <div class="store--btn">
+        <button class="menu--btn">Заказать расчёт</button>
+        <button class="service--btn">Список услуг</button>
+    </div>
+    </div>
+    
+    <div class="about">
+        <div class="abot--container">
+            <h1 class="about--title">Собственное производство с 2005 года</h1>
+            <h5 class="about--text">Работаем на новом современном оборудовании</h5>
+        </div>
+
+
+
+    </div>
+    
+
+    <div class="service">
+        <h1 class="service--title">Изготовление ёмкостей на заказ</h1>
+        <div class="service--block">
+        <div class="service--container">
+        <h5 class="service--text">Короткий перечень услуг:</h5>
+        <h3 class="service--item">Ёмкостное оборудование на заказ по вашим чертежам</h3>
+        <h3 class="service--item">Ёмкости горизонтальные и вертикальные</h3>
+        <h3 class="service--item">Ёмкости с рубашкой и змеевиком</h3>
+        <h3 class="service--item">Ёмкости с перемешивающими устройствами</h3>
+        <h3 class="service--item">Ёмкости с различной степенью автоматизации</h3>
+        <h3 class="service--item">Ёмкости с утеплением</h3>
+        <h3 class="service--item">Ёмкости с рубашкой охлаждения</h3>
+        <h3 class="service--item">Одностенные ёмкости</h3>
+        <h3 class="service--item">Пищевые ёмкости</h3>
+        <h3 class="service--item">Ёмкости для пивной продукции</h3>
+        <h3 class="service--item">Баки и котлы для бани</h3>
+        <h3 class="service--item">Услуги аргонодуговой сварки TIG</h3>
+        <h3 class="service--item">Нестандартные сварные конструкции</h3>
+    </div>
+        <img src="./img/2.jpg" alt="#" class="service--img">
+        </div>
+        <button class="menu--btn">Узнать цены</button>
+    </div>
+
+    <div class="conditions">
+        <h1 class="conditions--title">Условия сотрудничества?</h1>
+        <div class="conditions--items">
+            <div class="item--container">
+            <div class="conditions--item">
+                <img src="./img/blocks.webp" class="conditions--item__img">
+                <h3 class="condition--item__text">Серийные и штучные заказы</h3>
+            </div>
+            <div class="conditions--item">
+                <img src="./img/docs.webp" class="conditions--item__img">
+                <h3 class="condition--item__text">Скидки за объём</h3>
+            </div>
+            <div class="conditions--item">
+                <img src="./img/line.webp" class="conditions--item__img">
+                <h3 class="condition--item__text">По вашим чертежам</h3>
+            </div>
+        </div>
+            <div class="item--container">
+            <div class="conditions--item">
+                <img src="./img/pulp.webp" class="conditions--item__img">
+                <h3 class="condition--item__text">Делаем любые нестандартные конструкции</h3>
+            </div>
+            <div class="conditions--item">
+                <img src="./img/doc.webp" class="conditions--item__img">
+                <h3 class="condition--item__text">Гарантия 1 год </h3>
+            </div>
+            <div class="conditions--item">
+                <img src="./img/car.webp" class="conditions--item__img">
+                <h3 class="condition--item__text">Своя доставка с хорошими тарифами</h3>
+            </div>
+        </div>
+           
+        </div>
+    </div>
+
+    <div class="callback">
+        <h3 class="callback--title">Просто оставьте номер телефона и мы перезвоним</h3>
+        <p class="callback--text">Перезваниваем в среднем через 5 минут (в рабочие дни с 9 до 18)</p>
+         <div class="callback--form">
+            <div class="for__phone">
+            <p>Телефон<span>*</span></p>
+            <input type="phone" class="callback--input" placeholder="+7 (___)___-__-__">
+            </div>
+            <div class="form__name">
+            <p>Ваше имя<span>*</span></p>
+            <input type="text" class="callback--input" placeholder="Иван Петров">
+        </div>
+            
+         </div>
+         <button class="menu--btn">Перезвоните</button>
+            <p class="agreement">Нажимая "Перезвоните", даю согласие на обработку персональной информации <a href="#">Подробнее</a></p>
+    </div>
+
+    <div class="portfolio">
+        <h1 class="portolio--title">Некоторые наши работы</h1>
+        <h4 class="portfolio--text">Больше фотографий по запросу</h4>
+        <div class="portfolio--slider"></div>
+        <div class="portfolio--slider"></div>
+    </div>
+
+    <div class="questions">
+        <h1 class="questions--title">Остались вопросы?</h1>
+        <h3 class="questions--text">Напишите свой вопрос в форму ниже и мы ответим в течении 15 минут</h3>
+        <div class="questions--form">
+        <div class="form--item">
+            <p>Электронная почта<span>*</span></p>
+            <input type="email" placeholder="email@example.com" class="form">
+        </div>
+        <div class="form--item">
+            <p>Телефон</p>
+            <input type="phone" placeholder="+7 (___)___-__-__" class="form">
+        </div>
+        <div class="form--item">
+            <p>Ваше имя</p>
+            <input type="name" placeholder="Иван Петров" class="form">
+        </div>
+        <div class="form--item">
+            <p>Вопрос</p>
+            <input type="text" placeholder="В любом формате" class="form">
+        </div>
+            
+           
+        </div> 
+        <button class="menu--btn">Задать вопрос</button>
+            <p class="agreement">Нажимая "Задать вопрос", даю согласие на обработку персональной информации
+                <a href="#">Подробнее</a>
+            </p>
+    </div>
+
+    <div class="footer">
+        <div class="footer--list">
+            <div class="footer--menu">
+                <h3 class="footer--title">О компании</h3>
+                <p class="footer--text">Изготовление ёмкостей из нержавеющей стали</p>
+            </div>
+            <div class="footer--menu">
+                <h3 class="footer--title">Меню</h3>
+                <a href="#" class="footer--text">Производство</a>
+                <a href="#" class="footer--text">Цены</a>
+                <a href="#" class="footer--text">Услуги</a>
+                <a href="#" class="footer--text">Скидки</a>
+            </div>
+            <div class="footer--menu">
+                <h3 class="footer--title">Контакты</h3>
+                <button class="footer--btn">
+                    <img src="./img/98-981549_resultado-de-imagen-para-icono-telefono-blanco-phone.png" alt="" class="phone--img">
+                    Показать телефон</button>
+                <a href="#" class="footer--text">
+                    <img src="./img/clock.png" alt="" class="footer--img">Пн — Пт с 9:00 до 18:00
+                </a>
+                <a href="#" class="footer--text">
+                    <img src="./img/8666640_map_pin_icon-PhotoRoom.png" alt="" class="footer--img">г. Раменское, Северное шоссе 15</a>
+                <a href="#" class="footer--text">
+                    <img src="./img/email.png" alt="" class="footer--img" >svarmetal5@yandex.ru</a>
+            </div>
+        </div>
+        <div class="footer--rights">
+            <h3 class="rights--title">СВАРМЕТАЛЛ</h3>
+            <p class="rights--text">©Все права защищены 2005-2023</p>
+        </div>
+    </div>
+
+</div>
+
+</template>
+
+
+<script>
+import { VueAgile } from 'vue-agile'
+
+export default {
+  components: {
+    agile: VueAgile
+  }
+}
+</script>
+
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+h2, h3, h4, h5, h6 {
+    font-weight: 400;
+}
+
+.page {
+    max-width: 100%;
+    margin: auto;
+}
+
+/*Header--title*/
+
+.menu--logo {
+    width: 40px;
+    height: 50px;
+}
+
+.header--title {
+    display: flex;
+    align-items: center;
+    height: 50px;
+    width: 100%;
+    justify-content: center;
+    
+
+}
+.header--title::before {
+
+    position: absolute;
+    margin-top: 55px;
+    content: "";
+    border: 1px solid lightgray;
+    width: 100%;
+}
+
+.header--btn {
+    display: flex;
+    justify-content: space-around;
+    width: 180px;
+    height: 30px;
+    padding: 5px;
+    margin-left: 15px;
+    border: 1.5px solid black;
+    border-radius: 50px;
+    background-color: white;
+    cursor: pointer;
+
+
+}
+
+.phone--img {
+    width: 20px;
+    height: 20px;
+}
+
+.header--text {
+    display: flex;
+    margin-top: 10px;
+    margin-left: 20px;
+}
+
+.clock--img {
+    width: 25px;
+    height: 25px;
+    margin-right: 5px;
+    
+}
+
+
+
+/*Header Menu*/
+
+.header--menu {
+
+    display: flex;
+    margin-top: 15px;
+    align-items: center;
+    justify-content: center;
+    
+    
+}
+
+.menu--logo {
+    margin: 0 15px;
+}
+
+.menu--list {
+    display: flex;
+    justify-content: space-between;
+    margin-left: 100px;
+    margin-right: 75px;
+
+}
+
+.menu--item {
+    margin-right: 25px;
+    
+}
+
+.menu--btn {
+    
+    padding: 15PX;
+    border-radius: 5px;
+    background-color: rgb(253, 208, 74);
+    border: none;    
+    font-size: 20PX;
+    cursor: pointer;
+}
+
+.store {
+    background-image: url("./img/bg.jpg");
+    margin-top: 15px;
+    padding: 100px 0;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.store--text {
+    position: relative;
+    margin-top: 5px;
+    font-size: 25px;
+}
+
+.store--text::before {
+
+    position: absolute;
+    content: "";
+    border: 1px solid white;
+    left: -35px;
+    top: 13px;
+    width: 25px;
+}
+
+.store--btn {
+
+    display: flex;
+    margin-top: 100px;
+}
+
+.service--btn {
+    margin-left: 15px;
+    padding: 15px;
+    border: 1px solid white;
+    border-radius: 5px;
+    background: none;
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+}
+
+/*------------------------------- About---------------------*/
+
+
+.about {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+    
+    
+}
+
+.about--text {
+    
+    text-align: left;
+    margin-top: 15px;
+    font-size: 20px;
+}
+
+/*---------------Services------------------*/
+
+.service {
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 50px 0;
+    background-color: rgb(255, 248, 222);
+    font-size: 20px;
+    
+}
+
+.service--title {
+    display: flex;
+    align-items: start;
+    
+}
+
+
+.service--block {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-top: 10px;
+    margin-bottom: 50px;
+    
+}
+
+.service--text {
+    max-width: 450px;
+    margin-top: 10px;
+    font-size: 20px;
+}
+
+.service--item {
+    max-width: 450px;
+    margin-top: 30px;
+}
+
+.service--img {
+    
+    width: 460px;
+    height: 360px;
+    
+    
+}
+/*--------------------------Conditions--------------------*/
+
+
+.conditions {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+}
+
+.conditions--items {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+   
+    
+    
+
+}
+
+.item--container {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 50px;
+}
+
+.conditions--item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 250px;
+    margin-right: 50px;
+}
+
+.conditions--item__img {
+    width: 110px;
+    height: 110px;
+    
+}
+
+.condition--item__text {
+
+    max-width: 170px;
+}
+
+
+/*-----------Callback------------------------*/
+
+.callback {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 50px 0;
+    background-color: rgb(255, 248, 222);
+    
+    
+}
+
+.callback--title {
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 50px;
+}
+
+.callback--form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 50px 0;
+}
+
+.callback--input {
+    width: 500px;
+    height: 40px;
+    padding: 10px;
+}
+
+span {
+    color: red;
+}
+
+.agreement {
+    margin-top: 25px;
+    font-size: 12px;
+    color: lightgray;
+}
+
+
+
+/*----------------Questions--------------------*/
+
+.questions {
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+}
+
+.questions--text {
+    margin-top: 15px;
+    margin-bottom: 50px;
+}
+
+.questions--form {
+    
+}
+
+.form--item {
+    margin-bottom: 20px;
+
+}
+
+.form {
+    width: 500px;
+    height: 40px;
+    padding: 15px;
+    font-size: 15px;
+}
+
+
+/*----------Footer----------------------*/
+.footer {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(48, 48, 48);
+    color: white;
+}
+
+.footer--list {
+    display: flex;
+    justify-content: space-between;
+    padding: 50px 50px 20px 50px;
+
+    
+    
+    
+}
+
+.footer--menu {
+    display: flex;
+    flex-direction: column;
+    
+}
+
+.footer--text {
+    display: flex;
+    margin-top: 10px;
+    color: gray;
+    text-decoration: none;
+    max-width: 250px;
+    
+}
+
+.footer--text:hover {
+    border-bottom: 1px solid gray;
+    
+}
+
+.footer--btn {
+    background: none;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 180px;
+    height: 30px;
+    padding: 5px;
+    border: 1.5px solid white;
+    border-radius: 50px;
+    cursor: pointer;
+
+}
+
+
+.footer--rights {
+    display: flex;
+    justify-content: space-around;
+    padding: 20px 0;
+    border-top: 1px solid gray;
+}
+
+.rights--text {
+    color: gray;
+    
+}
+
+.footer--img {
+    margin-right: 10px;
+    width: 20px;
+    height: 20px;
+    
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import VueAgile from 'vue-agile'
+
+
+createApp(App).mount('#app').use(VueAgile)
